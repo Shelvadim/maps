40d5a7287715f6f5859b7705a8531397c1314afd
Shelgachev Vadim <Shelgachev@yahoo.com>
2021-04-06 11:15:57 -0700

    1st commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('hi there');
+
+
+//  git init.
+//git remote add origin https://github.com/Shelvadim/maps.git
+//git add -A
+//git commit -m "Message you would like to put"
+//git push origin master
